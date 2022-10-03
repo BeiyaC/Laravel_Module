9339ec586758cd6d4b065bb0a0ae1a5ca6d73bdd
BeiyaC <anis.grigah@le-campus-numerique.fr>
2022-10-03 15:45:44 +0200

    Correct return

--- app/Http/Controllers/Frontoffice/HomeController.php
+++ app/Http/Controllers/Frontoffice/HomeController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
-    public function show(): string
+    public function show()
     {
         return view('homepage');
     }
